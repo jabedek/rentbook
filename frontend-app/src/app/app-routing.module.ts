3acc30f56457a07c1b26a12cffd1a4d8c03a1066
Jan Bednarek <jabedek@gmail.com>
2020-07-06 14:01:21 +0200

    created basic routes

--- frontend-app/src/app/app-routing.module.ts
+++ frontend-app/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BooksComponent } from './components/rental/books/books.component';
+import { AddBookComponent } from './components/admin/add-book/add-book.component';
+import { LandingComponent } from './components/main/landing/landing.component';
+import { AboutComponent } from './components/about/about/about.component';
+import { HomeComponent } from './components/main/home/home.component';
 
-const routes: Routes = [{ path: '', component: BooksComponent }];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'rental', component: BooksComponent },
+  { path: 'admin/add-book', component: AddBookComponent },
+  { path: 'about', component: AboutComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend-app/src/app/app.module.ts
+++ frontend-app/src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BooksComponent } from './components/rental/books/books.component';
 import { BookItemComponent } from './components/rental/book-item/book-item.component';
 import { AddBookComponent } from './components/admin/add-book/add-book.component';
+import { HeaderComponent } from './components/common/layout/header/header.component';
+import { HomeComponent } from './components/main/home/home.component';
+import { LandingComponent } from './components/main/landing/landing.component';
+import { AboutComponent } from './components/about/about/about.component';
 
 @NgModule({
   declarations: [
@@ -15,8 +19,12 @@ import { AddBookComponent } from './components/admin/add-book/add-book.component
     BooksComponent,
     BookItemComponent,
     AddBookComponent,
+    HeaderComponent,
+    HomeComponent,
+    LandingComponent,
+    AboutComponent,
   ],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
