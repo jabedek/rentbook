93d990ff62a1d4ce70c8ea7b59e0c56b3a52dd6d
Jan Bednarek <jabedek@gmail.com>
2020-08-25 12:17:13 +0200

    testing and refactor

--- frontend-app/src/app/admin/admin-dashboard/admin-dashboard.component.ts
+++ frontend-app/src/app/admin/admin-dashboard/admin-dashboard.component.ts
@@ -12,15 +12,37 @@ import { AppState } from 'src/app/interfaces/app-state';
 export class AdminDashboardComponent implements OnInit {
   booksNumber: number = 0;
   usersNumber: number = 0;
+
+  bookLastAdded: string = '';
+  userLastAdded: string = '';
+  userEmail: string = '';
+  bookTitle: string = '';
+
   constructor(private crudService: CrudService) {}
+
+  setDetails(data: any[], tableName: string) {
+    if (tableName === 'users') {
+      this.usersNumber = data.length;
+      this.userLastAdded = data[data.length - 1].dateAdded;
+      this.userEmail = data[data.length - 1].email;
+    }
+
+    if (tableName === 'books') {
+      this.booksNumber = data.length;
+      this.bookLastAdded = data[data.length - 1].dateAdded;
+      this.bookTitle = data[data.length - 1].title;
+    }
+  }
 
   ngOnInit(): void {
     this.crudService
       .read('http://localhost:3000/books')
-      .subscribe((data) => (this.booksNumber = data.length));
+      // .subscribe((data) => (this.booksNumber = data.length));
+      .subscribe((data) => this.setDetails(data, 'books'));
 
     this.crudService
       .read('http://localhost:3000/users')
-      .subscribe((data) => (this.usersNumber = data.length));
+      // .subscribe((data) => (this.usersNumber = data.length));
+      .subscribe((data) => this.setDetails(data, 'users'));
   }
 }
